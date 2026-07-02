6c1ec64fbb27cf07df1f3f47e536ed1c35197e2e
german <germancitopino@gmail.com>
2026-07-02 14:35:38 -0400

    Refactor de las vistas de productos, usuarios y categorias

--- Control/funciones.php
+++ Control/funciones.php
@@ -1,37 +1,118 @@
-<?php 
-function get_row($table,$row, $id, $equal){
-	global $con;
-	$query=mysqli_query($con,"select $row from $table where $id='$equal'");
-	$rw=mysqli_fetch_array($query);
-	$value=$rw[$row];
-	return $value;
-}
-function guardar_historial($id_producto,$user_id,$fecha,$nota,$reference,$quantity){
-	global $con;
-	$sql="INSERT INTO historial (id_historial, id_producto, user_id, fecha, nota, referencia, cantidad)
-	VALUES (NULL, '$id_producto', '$user_id', '$fecha', '$nota', '$reference', '$quantity');";
-	$query=mysqli_query($con,$sql);
-	
-	
-}
-function agregar_stock($id_producto,$quantity){
-	global $con;
-	$update=mysqli_query($con,"update products set stock=stock+'$quantity' where id_producto='$id_producto'");
-	if ($update){
-			return 1;
-	} else {
-		return 0;
-	}	
-		
-}
-function eliminar_stock($id_producto,$quantity){
-	global $con;
-	$update=mysqli_query($con,"update products set stock=stock-'$quantity' where id_producto='$id_producto'");
-	if ($update){
-			return 1;
-	} else {
-		return 0;
-	}	
-		
-}
+<?php
+    session_start();
+    if (!isset($_SESSION['user_login_status']) && $_SESSION['user_login_status'] != 1) {
+        header("location: login.php");
+        exit;
+    }
+    
+    /* Connect To Database*/
+    require_once("config/db.php"); // Contiene las variables de configuracion para conectar a la base de datos
+    require_once("config/conexion.php"); // Contiene funcion que conecta a la base de datos
+    
+    $active_categoria = "active";
+    $title = "Categorías";
 ?>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <?php include("head.php");?>
+  </head>
+  <body>
+    <?php include("navbar.php"); ?>
+    
+    <div class="container">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <div class="btn-group pull-right">
+                    <button type='button' class="btn btn-success" data-toggle="modal" data-target="#nuevoCliente">
+                        <span class="glyphicon glyphicon-plus"></span> Nueva Categoría
+                    </button>
+                </div>
+                <h4><i class='glyphicon glyphicon-search'></i> Buscar Categorías</h4>
+            </div>
+            
+            <div class="panel-body">
+                <?php
+                include("modal/registro_categorias.php");
+                include("modal/editar_categorias.php");
+                ?>
+                
+                <form class="form-horizontal" role="form" id="datos_cotizacion">
+                    <div class="form-group row">
+                        <label for="q" class="col-md-2 control-label">Nombre</label>
+                        <div class="col-md-5">
+                            <input type="text" class="form-control" id="q" placeholder="Nombre de la categoría" onkeyup='load(1);'>
+                        </div>
+                        <div class="col-md-3">
+                            <button type="button" class="btn btn-default" onclick='load(1);'>
+                                <span class="glyphicon glyphicon-search"></span> Buscar
+                            </button>
+                            <span id="loader"></span>
+                        </div>
+                    </div>
+                </form>
+                
+                <div id="resultados"></div><div class='outer_div'></div></div>
+        </div>
+    </div>
+    
+    <hr>
+    <?php include("footer.php"); ?>
+    
+    <script type="text/javascript" src="js/categorias.js"></script>
+  </body>
+</html>
+
+<script>
+// Guardar nueva categoría de forma asíncrona
+$("#guardar_categoria").submit(function( event ) {
+    $('#guardar_datos').attr("disabled", true);
+  
+    var parametros = $(this).serialize();
+    $.ajax({
+        type: "POST",
+        url: "ajax/nueva_categoria.php",
+        data: parametros,
+        beforeSend: function(objeto){
+            $("#resultados_ajax").html("Mensaje: Cargando...");
+        },
+        success: function(datos){
+            $("#resultados_ajax").html(datos);
+            $('#guardar_datos').attr("disabled", false);
+            load(1);
+        }
+    });
+    event.preventDefault();
+});
+
+// Editar categoría existente
+$("#editar_categoria").submit(function( event ) {
+    $('#actualizar_datos').attr("disabled", true);
+  
+    var parametros = $(this).serialize();
+    $.ajax({
+        type: "POST",
+        url: "ajax/editar_categoria.php",
+        data: parametros,
+        beforeSend: function(objeto){
+            $("#resultados_ajax2").html("Mensaje: Cargando...");
+        },
+        success: function(datos){
+            $("#resultados_ajax2").html(datos);
+            $('#actualizar_datos').attr("disabled", false);
+            load(1);
+        }
+    });
+    event.preventDefault();
+});
+
+// Pasar los datos de la tabla al modal para poder editarlos
+function obtener_datos(id){
+    var nombre_categoria = $("#nombre_categoria"+id).val();
+    var descripcion_categoria = $("#descripcion_categoria"+id).val(); // Por si tienes descripción en tu tabla
+    
+    $("#mod_id").val(id);
+    $("#mod_nombre").val(nombre_categoria);
+    $("#mod_descripcion").val(descripcion_categoria);
+}
+</script>
--- Control/navbar.php
+++ Control/navbar.php
@@ -1,18 +1,21 @@
 <?php if (isset($title)) { ?>
 
-<header>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
+<header class="main-header">
 
+    <nav class="navbar navbar-default navbar-fixed-top">
+
+        <div class="container-fluid">
+
+            <!-- Logo -->
             <div class="navbar-header">
 
                 <button type="button"
                         class="navbar-toggle collapsed"
                         data-toggle="collapse"
-                        data-target="#menuPrincipal"
+                        data-target="#navbarEcoRoute"
                         aria-expanded="false">
 
-                    <span class="sr-only">Abrir menú</span>
+                    <span class="sr-only">Menú</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -20,34 +23,35 @@
                 </button>
 
                 <a class="navbar-brand" href="stock.php">
-                    <span class="glyphicon glyphicon-leaf"></span>
-                    EcoRoute AI
+                    <span class="glyphicon glyphicon-road"></span>
+                    <strong>EcoRoute Logistic AI</strong>
                 </a>
 
             </div>
 
-            <div class="collapse navbar-collapse" id="menuPrincipal">
+            <!-- Menú -->
+            <div class="collapse navbar-collapse" id="navbarEcoRoute">
 
                 <ul class="nav navbar-nav">
 
                     <li class="<?php echo isset($active_productos) ? $active_productos : ''; ?>">
                         <a href="stock.php">
-                            <span class="glyphicon glyphicon-list-alt"></span>
-                            Artículos
+                            <span class="glyphicon glyphicon-th-large"></span>
+                            Inventario
                         </a>
                     </li>
 
                     <li class="<?php echo isset($active_categoria) ? $active_categoria : ''; ?>">
                         <a href="categorias.php">
-                            <span class="glyphicon glyphicon-folder-open"></span>
-                            Rubros
+                            <span class="glyphicon glyphicon-bookmark"></span>
+                            Categorías
                         </a>
                     </li>
 
                     <li class="<?php echo isset($active_usuarios) ? $active_usuarios : ''; ?>">
                         <a href="usuarios.php">
                             <span class="glyphicon glyphicon-user"></span>
-                            Empleados
+                            Personal
                         </a>
                     </li>
 
@@ -55,10 +59,15 @@
 
                 <ul class="nav navbar-nav navbar-right">
 
+                    <li class="navbar-text">
+                        <span class="glyphicon glyphicon-time"></span>
+                        <?php echo date("d/m/Y"); ?>
+                    </li>
+
                     <li>
                         <a href="login.php?logout">
-                            <span class="glyphicon glyphicon-log-out"></span>
-                            Cerrar sesión
+                            <span class="glyphicon glyphicon-off"></span>
+                            Salir
                         </a>
                     </li>
 
@@ -67,7 +76,9 @@
             </div>
 
         </div>
+
     </nav>
+
 </header>
 
 <?php } ?>
